4a4bd6715a4171cfd30cb3de16533128bfadc095
Aixufey <jack_11728@msn.com>
2024-01-04 22:55:34 +0100

    Corrected checkbox testing flow

--- src/pages/tests/SummaryForm.test.jsx
+++ src/pages/tests/SummaryForm.test.jsx
@@ -1,23 +1,27 @@
-import { fireEvent, logRoles, render, screen } from '@testing-library/react';
+import { logRoles, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { describe } from 'vitest';
 import SummaryForm from '../summary/SummaryForm';
 
 describe('SummaryForm for button and checkbox', () => {
-    test('checkbox flow', () => {
+    // Setup for userEvent simulation - async/await
+    const user = userEvent.setup();
+
+    test('checkbox flow', async () => {
         const { container } = render(<SummaryForm />);
         logRoles(container);
 
         const checkBox = screen.getByRole('checkbox', { name: /terms and conditions/i });
         const button = screen.getByRole('button', { name: /confirm order/i });
 
-        // checkbox init state - button disabled
-        expect(checkBox).not.toBeChecked();
-        expect(button).toBeDisabled();
-
-        // click checkbox
-        fireEvent.click(checkBox);
+        // click checkbox - button enabled - checkbox checked
+        await user.click(checkBox);
         expect(checkBox).toBeChecked();
         expect(button).toBeEnabled();
-        
+
+        // click again - button disabled - checkbox unchecked
+        await user.click(checkBox);
+        expect(checkBox).not.toBeChecked();
+        expect(button).toBeDisabled();
     })
 });
