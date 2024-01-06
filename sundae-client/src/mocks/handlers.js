34ca9abd6db5189f0dc7c2434befe52a2ae4039b
Aixufey <jack_11728@msn.com>
2024-01-06 17:25:26 +0100

    setup mock response

--- sundae-client/src/mocks/handlers.js
+++ sundae-client/src/mocks/handlers.js
@@ -1,5 +1,7 @@
 import { http, HttpResponse } from "msw";
 
+// Handlers run over Node, not the browser to mock the server
+// https://mswjs.io/docs/integrations/node
 const handlers = [
     http.get("http://localhost:3030/scoops", () => {
         return HttpResponse.json([
